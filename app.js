40e307a49841b336721a9e3e36491fab294936d7
Mohit Garg <gargmohit@live.in>
2017-04-26 22:14:40 +0530

    environment port

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.use(indexRoutes)
 app.use('/campgrounds/:id/comments',commentRoutes)
 app.use('/campgrounds',campgroundRoutes)
 
-app.listen(3000, function () {
-    console.log('Server is running on port 3000')
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The YelpCamp Server Has Started!");
 });
