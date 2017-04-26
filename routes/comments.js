67ca671a6fbbf68062d1f710b15afa40ee899edb
Mohit Garg <gargmohit@live.in>
2017-04-26 16:20:06 +0530

    comment delete route added

--- routes/comments.js
+++ routes/comments.js
@@ -38,7 +38,7 @@ router.post('/',isLoggedIn,function (req, res) {
     })
 })
 // Edit Comment
-router.get('/:comment_id/edit', function (req, res) {
+router.get('/:comment_id/edit',checkCommentOwnership, function (req, res) {
     Comment.findById(req.params.comment_id, function (err, foundComment) {
         if(err){
             res.redirect('back')
@@ -49,7 +49,7 @@ router.get('/:comment_id/edit', function (req, res) {
 })
 
 // Update Comment Route
-router.put('/:comment_id', function (req, res) {
+router.put('/:comment_id',checkCommentOwnership, function (req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
         if(err){
             res.redirect('back')
@@ -58,7 +58,34 @@ router.put('/:comment_id', function (req, res) {
         }
     })
 })
+// delete Campground
+router.delete('/:comment_id',checkCommentOwnership, function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        if(err){
+            res.redirect('back')
+        } else {
+            res.redirect('/campgrounds/'+ req.params.id)
+        }
+    })
+})
 
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function (err, foundComment) {
+            if (err) {
+                res.redirect('back')
+            } else {
+                if (foundComment.author.id.equals(req.user.id)) {
+                    next()
+                } else {
+                    res.redirect('back')
+                }
+            }
+        })
+    } else {
+        res.redirect('back')
+    }
+}
 // Middleware
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()){
